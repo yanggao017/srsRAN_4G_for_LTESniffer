216a9ca91cc055f66f638dc0fc9487b902b85840
yang gao <yang_gao@bupt.edu.cn>
2025-12-09 16:40:23 +0800

    remove print

--- .history/lib/src/phy/phch/ra_dl_20251101150940.c
+++ .history/lib/src/phy/phch/ra_dl_20251101150940.c
@@ -327,7 +327,7 @@ int srsran_dl_fill_ra_mcs(srsran_ra_tb_t* tb, int last_tbs, uint32_t nprb, bool 
 
   // Get Transport block size index
   int i_tbs = srsran_ra_tbs_idx_from_mcs(tb->mcs_idx, pdsch_use_tbs_index_alt, false);
-  //printf("DL DCI: MCS=%d -> Mod=%s, TBS_idx=%d\n", tb->mcs_idx, srsran_mod_string(tb->mod), i_tbs);
+  printf("DL DCI: MCS=%d -> Mod=%s, TBS_idx=%d\n", tb->mcs_idx, srsran_mod_string(tb->mod), i_tbs);
   // If i_tbs = -1, TBS is determined from the latest PDCCH for this TB (7.1.7.2 36.213)
   int tbs = 0;
   if (i_tbs >= 0) {
@@ -411,7 +411,7 @@ static int dl_dci_compute_tb(bool pdsch_use_tbs_index_alt, const srsran_dci_dl_t
       if (grant->tb[i].enabled) {
         grant->tb[i].tbs = srsran_dl_fill_ra_mcs(&grant->tb[i], grant->last_tbs[i], n_prb, pdsch_use_tbs_index_alt);
         //test yg print
-        //printf("DL DCI: Setting TBS=%d prb=%d\n", grant->tb[i].tbs, n_prb);
+        printf("DL DCI: Setting TBS=%d prb=%d\n", grant->tb[i].tbs, n_prb);
         if (grant->tb[i].tbs < 0) {
           char str[128];
           srsran_dci_dl_info(dci, str, sizeof(str));
